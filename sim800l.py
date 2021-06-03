09a263590a14a50fa486e0b0b08403d84ce3009f
Nishad Mandlik <mandliksg@gmail.com>
2021-06-04 00:42:59 +0200

    Intermediate

--- sim800l.py
+++ sim800l.py
@@ -8,6 +8,7 @@ Created on Thu Oct 15 00:23:55 2020
 
 import serial
 
+
 class SIM800L():
     """Class interface for SIM800L module
 
@@ -25,6 +26,25 @@ class SIM800L():
         self.port = port
         self.baudrate = baud
 
+    def __send(msg, resp=None, timeout=5):
+        """Send the message and match the reply with the given response
+
+        Parameters:
+            msg (str): Message to be sent to the module.
+            resp (str, optional): Response to expect from the module
+                                  If None, response will not be read
+            timeout (int, optional): Timeout (in sec) for the read operation.
+                                     This parameter is only considered if
+                                     'resp' is not None
+
+        """
+        self.reset_output_buffer()
+        self.write(msg.encode() + b"\n")
+        if resp is not None:
+            self.timeout = timeout
+            self.reset_input_buffer()
+            self.read()
+
     def send_sms(tel_num):
         """Sends text SMS to the given telephone number
 
@@ -34,7 +54,7 @@ class SIM800L():
 
         """
         self.open()
-            
+        self.write(b"")
         self.close()
 
     def add_lock(new_pin, old_pin=None)
